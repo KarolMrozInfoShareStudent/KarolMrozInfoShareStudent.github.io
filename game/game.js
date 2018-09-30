77f902c4b67664a9477773bb950d6c173e6a7c02
KarolMrozInfoShareStudent <polityk357@gmail.com>
2018-09-30 20:57:51 +0200

    przeniesienie piłki dp funkcji wywołanie fiunkcji na koncu ważne

--- game/game.js
+++ game/game.js
@@ -4,23 +4,31 @@ const ctx = canv.getContext('2d');
 
 canv.width = 1000;
 canv.height = 500;
+const piłkaRozmiar = 20;
 
 
 const canvWidth = canv.width;
 const canvHeight = canv.height;
 
-function table(){
+function table() {
     ctx.fillStyle = '#fff';//fil style koloruje uzywamy tyych samych wartości co w css
-    ctx.fillRect(0,0, canvWidth, canvHeight);
+    ctx.fillRect(0, 0, canvWidth, canvHeight);
 }
 //fillRect jest właściwością wymaga podania czterech argumetóœ, rysowanie w canvasie opiera sie na osi X pozima(od lewej do prawej)
 // i osi Y (od góry do dołu) najpierw x y początek póżniej xy koniec rysowania
 // zamiast wpisywać na sztywno wymiar lepiej podać zmienną z wysokością
 
 
-table()
-//piłka
-const piłka = 20;
 
-ctx.fillStyle = 'black';//fil style koloruje uzywamy tyych samych wartości co w css
-ctx.fillRect(canvWidth/ 2 - piłka,canvHeight /2 - piłka, piłka, piłka);
+//piłka
+
+function piłka() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(canvWidth / 2 - piłkaRozmiar, canvHeight / 2 - piłkaRozmiar, piłkaRozmiar, piłkaRozmiar);
+}
+
+
+
+table()
+
+piłka()
