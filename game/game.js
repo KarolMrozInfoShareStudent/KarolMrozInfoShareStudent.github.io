db871ae86eb744de53752dab3ccd218bd984617e
KarolMrozInfoShareStudent <polityk357@gmail.com>
2018-09-30 20:10:42 +0200

    added style body

--- game/game.js
+++ game/game.js
@@ -2,5 +2,10 @@ const canv = document.querySelector("#plansza");
 
 const ctx = canv.getContext('2d');
 
+canv.width = 1000;
+canv.height = 500;
 
+
+const canvWidth = canv.width;
+const canvHeight = canv.height;
 
