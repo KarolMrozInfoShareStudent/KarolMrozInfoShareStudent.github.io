4374578aa467f8b2ce06588bb1b4968c595362ba
KarolMrozInfoShareStudent <polityk357@gmail.com>
2018-09-30 20:25:30 +0200

    dodanie próbnego tła do planszy patrz table

--- game/game.js
+++ game/game.js
@@ -9,3 +9,11 @@ canv.height = 500;
 const canvWidth = canv.width;
 const canvHeight = canv.height;
 
+function table(){
+    ctx.fillStyle;
+    ctx.fillRect(0,0, 250, 250);
+}
+
+table()
+//fillStyle jest właściwością, rysowanie w canvasie opiera sie na osi X pozima(od lewej do prawej)
+// i osi Y (od góry do dołu)
